84b485128fea930f8e6e37af3c9bf0ba375ee5e9
Christelle Alexia <christellealexia53@gmail.com>
2023-02-17 07:32:48 -0800

    random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,7 +18,7 @@ int main(void)
         if (n>0)
         printf("%d is positive",n);
         else if(n<0)
-		(printf("%d is negative",n);
+		printf("%d is negative",n);
          else
        
       	 printf("%d is zero",n);
