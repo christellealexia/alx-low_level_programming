5164811b9e612a338db894638f16c32ec9bd3d97
Christelle Alexia <christellealexia53@gmail.com>
2023-02-17 05:51:46 -0800

    random number

--- 0x00-hello_world/0-positive_or_negative.c
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -10,6 +10,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	if (n>0){
+		printf("is positive");
+		else if(n<0){(printf("is negative");
+				else
+				printf("is zero");
 	/* your code goes there */
-	return (0);
+	return (0)
 }
