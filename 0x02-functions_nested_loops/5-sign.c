2243042234193cd0adb562ed0541250f62e329f2
christellealexia <christellealexia53@gmail.com>
2023-02-21 23:08:31 +0200

    sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _print_sign- printing sign of numbers
+ * @n: integer n to check
+ * Return: int
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+	return (1);
+	}
+	else if (n == 0)
+	{
+	return (0);
+	}
+	else
+	{
+	return (-1);
+	}
+}
