644332f96dfadf6a024ef9da17e23fe960cf2a84
christellealexia <christellealexia53@gmail.com>
2023-02-21 20:14:37 +0200

    putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,11 @@ void print_alphabet_x10(void)
 {
 	char lowercase = 'a';
 
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j < 26; j++) 
+	for (j = 0; j < 26; j++)
 	{
 	putchar (lowercase);
 	putchar ('\n');
