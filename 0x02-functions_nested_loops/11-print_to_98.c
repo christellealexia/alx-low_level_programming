9331d159ffbab29f420ed985a9f5ed08892ca624
christellealexia <christellealexia53@gmail.com>
2023-02-22 00:10:34 +0200

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_to_98- print natural number to 98
+ *@n: integer n to be used
+ *Return: int
+ */
+void print_to_98(int n)
+{
+	int n = 1;
+	for (n == 1; n <= 98; n++)
+	{
+	printf("%d", n);
+	}
+	printf('\n');
+}
