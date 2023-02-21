88802b9e1c17a21375a62d7aa94caaeb50a77f73
christellealexia <christellealexia53@gmail.com>
2023-02-21 23:14:34 +0200

    checking for the sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,24 @@
 #include "main.h"
 /**
- * _print_sign- printing sign of numbers
- * @n: integer n to check
- * Return: int
+ *_print_sign- printing sign of numbers
+ *@n: integer n to check
+ *Return: int
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	return (1);
+	_putchar('+');		
+	return (+1);
 	}
 	else if (n == 0)
 	{
+	_putchar('0');
 	return (0);
 	}
 	else
 	{
+	_putchar('-');
 	return (-1);
 	}
 }
