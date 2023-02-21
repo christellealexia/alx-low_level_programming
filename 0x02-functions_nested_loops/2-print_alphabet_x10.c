cffa45e6b30135b192b75b3279ca64234fd4df63
christellealexia <christellealexia53@gmail.com>
2023-02-21 18:09:31 +0200

    1o times an alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main -ENtry point
+ * Descrption: 'alphabets in lower case 10 times'
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char lowercase = 'a';
+
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 0; j < 26; j++) 
+	{
+	putchar (lowercase);
+	putchar ('\n');
+	lowercase++;
+	}
+	lowercase = 'a';
+	}
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
