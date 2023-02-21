f1968eb6ef9c9414697222cd57b5d641a83ad8fc
christellealexia <christellealexia53@gmail.com>
2023-02-22 01:03:25 +0200

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
  *print_last_digit- printing the last digit
  *Return: int
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	int lastDigit = n % 10;
 	printf("The last digit of %d is %d\n", n, lastDigit);
