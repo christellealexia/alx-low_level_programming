62ebd799cd651ffa624061bf6dad21455042cb56
christellealexia <christellealexia53@gmail.com>
2023-02-21 17:00:51 +0200

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
-	 char string[] = "_putchar";
-        int i=0;
-        for (i=0; string[i] != '\0'; i++)
-        {
-        _putchar (string[i]);
-        }
-        _putchar('\n');
-        return 0;
+{
+	char string[] = "_putchar";
+	int i = 0;
+	for (i = 0; string[i] != '\0'; i++)
+	{
+	_putchar (string[i]);
+	}
+	_putchar('\n');
+	return (0);
 
 }
