d6f5066dfb24569dbed9a6b47c31001fe28fd1c5
christellealexia <christellealexia53@gmail.com>
2023-02-22 17:12:16 +0200

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 	{
 	for (j = n; j >= 98; j--)
 	{
-	printf("%d", j);
+	printf("%d, ", j);
 	
 	}
 	}
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	{
 	for (i = n; i <= 98; i++)
 	{
-	printf("%d", i);
+	printf("%d, ", i);
 	}
 	printf("\n");
 	}
