3fd15887284dd2156e26ff333721665ec76866b9
christellealexia <christellealexia53@gmail.com>
2023-02-23 19:56:49 +0200

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main- fizz buzz
+ * @i: integer to use
+ * Return: int
+ */
+int main(void)
+{
+	int i;
+	for (i=1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+		return (0);
+}
+
