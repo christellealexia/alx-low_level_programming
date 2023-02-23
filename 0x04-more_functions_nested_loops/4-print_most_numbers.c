e3793546f40896b14400438fe41e087c4ec9013a
christellealexia <christellealexia53@gmail.com>
2023-02-24 00:39:51 +0200

    printing from 0 t0 9 skipping 2 an 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers- printing numbers from 0 to 9 but skipping 2 and 4
+ * @ a; a integer to be used
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int a;
+	for (a = 0; a <= 9; a++)
+	{
+		if (a == 2 && a == 4)
+		{
+			break;
+		}
+		else
+		{
+		_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
+
