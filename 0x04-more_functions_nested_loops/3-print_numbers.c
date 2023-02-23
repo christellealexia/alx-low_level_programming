7474eac42301f9870626692dc37e97295cd9428f
christellealexia <christellealexia53@gmail.com>
2023-02-24 00:21:39 +0200

    putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers- print numbers from 0 to 9
+ * @ c: integer c to be used
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for ( c = 0; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
