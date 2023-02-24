a0ada46173d89fb8ac6e94f59fdd3032a8eb6342
christellealexia <christellealexia53@gmail.com>
2023-02-24 11:32:28 +0200

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,28 +8,21 @@
 
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i=1; i <= 100; i++)
+	for (; i < 100 ; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%d ", i);
-		}
 	}
-	printf("\n");
+	printf("Buzz\n");
+
 	return (0);
 }
 
