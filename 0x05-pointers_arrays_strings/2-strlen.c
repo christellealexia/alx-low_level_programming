23a20fa9f7a93950e8e02e27dfa2288c9b285a63
christellealexia <christellealexia53@gmail.com>
2023-02-28 01:39:17 +0200

    returning length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen- returning the lengt of the string
+ * @ s:
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int arr[i], int count;
+
+
+	for (i=0; *(arr+i)='\0'; i++)
+	{
+		count= *(arr+i)
+			_putchar(count);
+	}
+	return (0);
+}
+
