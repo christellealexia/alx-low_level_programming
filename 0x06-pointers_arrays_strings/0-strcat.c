634a63729781785d591b23d4c71141e7690369cd
christellealexia <christellealexia53@gmail.com>
2023-03-02 15:27:29 +0200

    main file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat- concatinating two strings
+ * @dest: destination
+ * @src: source
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+	while (*p != '\0')
+	{
+	p++;
+	}
+	while (*src != '\0') {
+	*p++ = *src++;
+	}
+	*p = '\0';
+	return dest;
+}
