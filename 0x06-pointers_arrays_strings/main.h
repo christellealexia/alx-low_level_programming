36b5fab6d92bf282f81e4102b41ceb18d40c90da
christellealexia <christellealexia53@gmail.com>
2023-03-03 02:24:06 +0200

    updates

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,10 +2,16 @@
 #define main_h
 
 int _putchar(char c);
+
 char *_strcat(char *dest, char *src);
+
 char *string_toupper(char *);
+
 int _strcmp(char *s1, char *s2);
+
 void print_number(int n);
+
 char *rot13(char *);
+
 #endif
 
