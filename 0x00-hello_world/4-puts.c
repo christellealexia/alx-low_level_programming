caf7ff2996c43d527fdf764a696b32ecf0202e3f
christellealexia <christellealexia53@gmail.com>
2023-03-06 00:29:55 +0200

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
  * main- printing using puts
- * @Mystr: character used
+ * @mystr: character used
  * Return: Always 0
- * */
+ */
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
 
-	puts(Mystr);
+	puts(mystr);
 	return (0);
 }
