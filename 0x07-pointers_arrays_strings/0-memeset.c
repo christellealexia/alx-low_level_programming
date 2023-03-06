625cddde0c80891445f7d5b4d87326b70ad60816
christellealexia <christellealexia53@gmail.com>
2023-03-06 16:22:47 +0200

    filling the memort area

---
+++ 0x07-pointers_arrays_strings/0-memeset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memeset- fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: consant byte to fill in the memory area
+ * @n: number of bytes to be filled
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
