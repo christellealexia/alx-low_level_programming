22f5ae50cd37a004b16312f1e2213e01bfbaf6ff
christellealexia <christellealexia53@gmail.com>
2023-03-06 18:07:08 +0200

    locating a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr- locating a character in a string
+ * @s: string
+ * @c: character to be checked
+ * Return: a pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (c == s[i]) 
+		{
+		return (s+i);
+		}
+		else 
+		{
+		return (0);
+		}
+	}
+}
+
