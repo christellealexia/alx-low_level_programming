e6e1679adc9a54e713b55c8f026522ea572dcb32
christellealexia <christellealexia53@gmail.com>
2023-03-23 12:48:22 +0200

    header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef _VAR_FUNC_
+#define _VAR_FUNC_
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+#endif
+
